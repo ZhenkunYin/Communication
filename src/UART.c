078a86c2cf655fa417ee4fe328c048ce854b6445
ZhenkunYin <81162437+ZhenkunYin@users.noreply.github.com>
2022-07-24 22:25:28 +1000

    fixed bugs for receiveing and transmitting

--- src/UART.c
+++ src/UART.c
@@ -6,7 +6,13 @@
  */
 #include "Communication.h"
 
-static uint8_t uart_rec_buf[256];
+#define errorMsg "An error occurred! The application will stop!\r\n"
+#define BUFFER_SIZE 256U
+#define TIMEOUT 10U
+
+uint8_t bufferIdx;
+
+uint8_t uart_rec_buf[BUFFER_SIZE];
 
 extern QueueHandle_t que;
 
@@ -16,11 +22,16 @@ extern QueueHandle_t que;
 static void uart_callback(void *driverState, uart_event_t event, void *userData){
 	(void) userData;
 	(void) driverState;
-	LPUART_DRV_ReceiveData(INST_LPUART1,uart_rec_buf,256);
+	//LPUART_DRV_ReceiveData(INST_LPUART1,uart_rec_buf,256);
 	if(event == UART_EVENT_RX_FULL){
-		LPUART_DRV_SendData(INST_LPUART1,"this is interrupt\n",20);
-		//xQueueSend(que,(void *)uart_rec_buf,100);
-
+		//LPUART_DRV_SendData(INST_LPUART1,"INT1",4);
+		/* The reception stops when newline is received or the buffer is full */
+		if ((uart_rec_buf[bufferIdx] != '\n') && (bufferIdx != (BUFFER_SIZE - 2U))){
+			//LPUART_DRV_SendData(INST_LPUART1,"INT2",4);
+			/* Update the buffer index and the rx buffer */
+			bufferIdx++;
+			LPUART_DRV_SetRxBuffer(INST_LPUART1, &uart_rec_buf[bufferIdx], 1U);
+		}
 	}
 }
 
@@ -40,14 +51,33 @@ bool test = false;
  */
 static void UART_recieve_task(void *pvParameters){
 	(void) pvParameters;
+	uint32_t bytesRemaining;
+	status_t status;
 
-	const TickType_t UART_task_delay = pdMS_TO_TICKS(500UL);
+	const TickType_t UART_task_delay = pdMS_TO_TICKS(50UL);
 	LPUART_DRV_SendData(INST_LPUART1,"UART init",10);
 
 	while(1){
-		//LPUART_DRV_ReceiveDataBlocking(INST_LPUART1,uart_rec_buf,256,100);
+		LPUART_DRV_ReceiveData(INST_LPUART1,uart_rec_buf,1U);
+
+		while(LPUART_DRV_GetReceiveStatus(INST_LPUART1,&bytesRemaining) == STATUS_BUSY);
+
+		status = LPUART_DRV_GetReceiveStatus(INST_LPUART1, &bytesRemaining);
+
+		if(status != STATUS_SUCCESS){
+			LPUART_DRV_SendDataBlocking(INST_LPUART1, (uint8_t *)errorMsg, strlen(errorMsg), TIMEOUT);
+			break;
+		}
+
+		bufferIdx++;
+		uart_rec_buf[bufferIdx] = 0U;
+
+	    LPUART_DRV_SendData(INST_LPUART1, uart_rec_buf, bufferIdx);
+
+	    bufferIdx = 0U;
+
 		//if(strstr(uart_rec_buf,"11\n")){
-			LPUART_DRV_SendData(INST_LPUART1,"this is test\n",14);
+		//LPUART_DRV_SendData(INST_LPUART1,"this is test\n",14);
 		//}
 		//memset(uart_rec_buf,0,256);
 		//LPUART_DRV_SendData(INST_LPUART1,uart_rec_buf,256);
@@ -67,6 +97,6 @@ void UART_task_setup(){
 	        		"UART",
 	    			configMINIMAL_STACK_SIZE,
 	    			NULL,
-					0,//UART_RECEIVE_TASK_PRIORITY,
+					UART_RECEIVE_TASK_PRIORITY,
 	    			NULL);
 }
--- src/flexcan.c
+++ src/flexcan.c
@@ -17,7 +17,14 @@ static flexcan_data_info_t data_info = {
 	.is_remote = false
 };
 
+static message_type start_up_message={
+		.mb_data = {0x00,0x01,0x00,0x00,0x00,0x00,0x00,0x00},
+		.msg_id = 0x00,
+		.timeout_ms = 10
+};
+
 static flexcan_msgbuff_t recvMsg0;		//FlexCAN receive buffer
+flexcan_id_table_t id_filter_table[8];
 
 extern message_type driving_dynamics_1;
 extern message_type driving_dynamics_2;
@@ -38,18 +45,22 @@ void can_callback(uint8_t instance, flexcan_event_type_t eventType,
 	(void) buffIdx;
 	(void) flexcanState;
 
-	if(eventType == FLEXCAN_EVENT_RX_COMPLETE) {
-		if(instance == INST_CANCOM0) { //Receive data from can0 into recvmsg0, and send it
-
-			FLEXCAN_DRV_Receive(INST_CANCOM0, RX_MAILBOX_CAN0, &recvMsg0);
-			recvMsg0.data[1]=0xFF;
-			FLEXCAN_DRV_Send(INST_CANCOM0, TX_MAILBOX_CAN0, &data_info, recvMsg0.msgId, recvMsg0.data);
+	if(eventType == FLEXCAN_EVENT_DMA_COMPLETE){
+		if(recvMsg0.msgId == 0x18C){
+			if(recvMsg0.data[1] == 0x00){
+				/*emergency*/
+				LPUART_DRV_SendData(INST_LPUART1,"emergency\n",12);
+			}else if(recvMsg0.data[1] == 0x03){
+				/*start up*/
+				LPUART_DRV_SendData(INST_LPUART1,"start up\n",10);
+			}else if(recvMsg0.data[1] == 0x01){
+				/*ready*/
+				LPUART_DRV_SendData(INST_LPUART1,"ready\n",7);
+			}
+		}else if(recvMsg0.msgId == 0x70C){
+			LPUART_DRV_SendData(INST_LPUART1,"init\n",6);
+			FLEXCAN_DRV_Send(INST_CANCOM0, 9U, &data_info, start_up_message.msg_id, start_up_message.mb_data);
 		}
-	}else if(eventType == FLEXCAN_EVENT_DMA_COMPLETE){
-		if(recvMsg0.data[0] == 0x03){
-			recvMsg0.data[1] == 0xCC;
-		}
-		FLEXCAN_DRV_Send(INST_CANCOM0, TX_MAILBOX_CAN0, &data_info, recvMsg0.msgId, recvMsg0.data);
 		FLEXCAN_DRV_RxFifo(INST_CANCOM0,&recvMsg0);
 	}
 }
@@ -60,54 +71,55 @@ void can_callback(uint8_t instance, flexcan_event_type_t eventType,
 static void flexcan_init(){
 	FLEXCAN_DRV_Init(INST_CANCOM0, &canCom0_State, &canCom0_InitConfig0);
 
-	flexcan_id_table_t id_filter_table = {
-		.isRemoteFrame = false,      /*!< Remote frame*/
-		.isExtendedFrame = false,    /*!< Extended frame*/
-		.id = 0x18C
-	};
+	for(int i=0; i<2; i++){
+		id_filter_table[i].isRemoteFrame = false;      /*!< Remote frame*/
+		id_filter_table[i].isExtendedFrame = false;    /*!< Extended frame*/
+	}
 
-	//FLEXCAN_DRV_ConfigRxFifo(INST_CANCOM0,FLEXCAN_RX_FIFO_ID_FORMAT_A,&id_filter_table);
+	id_filter_table[0].id = 0x18C;
+	id_filter_table[1].id = 0x70C;
 
-	FLEXCAN_DRV_ConfigRxMb(INST_CANCOM0,RX_MAILBOX_CAN0,&data_info,0x500);
-
+	FLEXCAN_DRV_ConfigRxFifo(INST_CANCOM0,FLEXCAN_RX_FIFO_ID_FORMAT_A,id_filter_table);
 	FLEXCAN_DRV_InstallEventCallback(INST_CANCOM0,can_callback,NULL);
-
+	EDMA_DRV_Init(&dmaController1_State, &dmaController1_InitConfig0, edmaChnStateArray, edmaChnConfigArray, EDMA_CONFIGURED_CHANNELS_COUNT);
 	//FLEXCAN_DRV_SetRxMbGlobalMask(INST_CANCOM0,FLEXCAN_MSG_ID_STD,0xFFF);
 	//FLEXCAN_DRV_RxFifo(INST_CANCOM0,&recvMsg0);
 }
 
-/**
- * @brief sending messages
- *
- * @param	message: message data, ID, timeout
+/**TASK************************************************************************
+ * @brief messages sending task
  */
-static void send_messages(const message_type message){
-	FLEXCAN_DRV_Send(INST_CANCOM0,TX_MAILBOX_CAN0,&data_info,message.msg_id,message.mb_data);
+static void message_sending_task(void *pvParameters){
+	(void) pvParameters;
+
+	const TickType_t flexcan_task_delay = pdMS_TO_TICKS(20UL);
+
+	while(1){
+
+		vTaskDelay(flexcan_task_delay);
+	}
 }
 
 
 extern bool test;
 
-/**
+/**TASK************************************************************************
  * @brief task content
  */
-void message_sending_task(void *pvParameters){
+void message_monitoring_task(void *pvParameters){
 	(void) pvParameters;
 
-	const TickType_t flexcan_task_delay = pdMS_TO_TICKS(1000UL);
+	const TickType_t flexcan_task_delay = pdMS_TO_TICKS(50UL);
+
+	status_t status;
 
 	while(1){
-		/*if(FLEXCAN_DRV_ReceiveBlocking(INST_CANCOM0, RX_MAILBOX_CAN0, &recvMsg0,100) == STATUS_SUCCESS){
-			recvMsg0.data[1] = 0xFF;
-			FLEXCAN_DRV_Send(INST_CANCOM0,TX_MAILBOX_CAN0,&data_info,recvMsg0.msgId,recvMsg0.data);
-		}*/
-		if(test){
-			test=false;
-			send_messages(driving_dynamics_1);
+		status = FLEXCAN_DRV_RxFifo(INST_CANCOM0,&recvMsg0);
+
+		if(status == STATUS_SUCCESS){
+
 		}
 
-		//driving_dynamics_1.mb_data[0]++;
-		//send_messages(driving_dynamics_1);
 		vTaskDelay(flexcan_task_delay);
 	}
 }
@@ -118,10 +130,17 @@ void message_sending_task(void *pvParameters){
 void FlexCAN_task_setup(){
 	flexcan_init();
 
-	xTaskCreate(message_sending_task,
-	    		"Flexcan",
+	xTaskCreate(message_monitoring_task,
+	    		"RES monitor",
 				configMINIMAL_STACK_SIZE,
 				NULL,
-				FLEXCAN_TASK_PRIORITY,
+				MONITORING_TASK_PRIORITY,
 				NULL);
+
+	/*xTaskCreate(message_sending_task,
+		    	"CANbus sending",
+				configMINIMAL_STACK_SIZE,
+				NULL,
+				CANBUS_SENDING_TASK_PRIORITY,
+				NULL);*/
 }
